25615810bb98f50451e4ae969a6318e16c7dbb36
Thanatossan <kotokikung@gmail.com>
2020-01-06 10:15:52 +0700

    add medal controller

--- controllers/matchController.js
+++ controllers/matchController.js
@@ -1,51 +1,98 @@
-const database = require('../config/database')
+const database = require("../config/database");
 
-module.exports =  {
-    getMatch (req,res,next){
-        database.query('SELECT * FROM `match` ORDER BY `match`.`updated` DESC',(err,data)=>{
-            console.log(err);
-            res.json(data);
-        })
-    },
-    getIncoming(req,res,next){
-        database.query('SELECT * FROM `incomingmatch` ORDER BY `incomingmatch`.`startTime` ASC',(err,data)=>{
-            res.json(data);
-        })
-    },
-    addMatch(req,res,next){
-        const data = req.body.data;
-        console.log(data);
-        database.query(`select judgeID from judge where username = '${data.judgeID.username}'`,(err,judge)=>{
-            if(judge[0].judgeID  === undefined) {
-                console.log("session expired");
+module.exports = {
+  getMatch(req, res, next) {
+    database.query(
+      "SELECT * FROM `match` ORDER BY `match`.`updated` DESC",
+      (err, data) => {
+        console.log(err);
+        res.json(data);
+      }
+    );
+  },
+  getIncoming(req, res, next) {
+    database.query(
+      "SELECT * FROM `incomingmatch` ORDER BY `incomingmatch`.`startTime` ASC",
+      (err, data) => {
+        res.json(data);
+      }
+    );
+  },
+  addMatch(req, res, next) {
+    const data = req.body.data;
+    console.log(data);
+    database.query(
+      `select judgeID from judge where username = '${data.judgeID.username}'`,
+      (err, judge) => {
+        if (judge[0].judgeID === undefined) {
+          console.log("session expired");
+        } else {
+          let id = judge[0].judgeID;
+          database.query(
+            `select fieldNumber from field where '${data.sportField}' = fieldName`,
+            (err, fieldNum) => {
+              let field;
+              if (fieldNum.length < 0) field = null;
+              else field = parseInt(fieldNum[0].fieldNumber);
+              database.query(
+                "INSERT INTO `match`(`judgeID`, `nameTeam1`, `nameTeam2`, `nameTeam3`, `scoreTeam1`, `scoreTeam2`, `scoreTeam3`, `team1Player`, `team2Player`, `team3Player`, `sportType`, `sportCategory`, `fieldNumber`, `matchStartTime`, `matchEndTime`) VALUES (" +
+                  id +
+                  ',"' +
+                  data.nameTeam1 +
+                  '","' +
+                  data.nameTeam2 +
+                  '","' +
+                  data.nameTeam3 +
+                  '","' +
+                  data.scoreTeam1 +
+                  '" ,"' +
+                  data.scoreTeam2 +
+                  '","' +
+                  data.scoreTeam3 +
+                  '","' +
+                  data.player1 +
+                  '","' +
+                  data.player2 +
+                  '","' +
+                  data.player3 +
+                  '","' +
+                  data.sportType +
+                  '","' +
+                  data.sportCategory +
+                  '",' +
+                  field +
+                  ',"' +
+                  data.startTime +
+                  '" ,"' +
+                  data.endTime +
+                  '")'
+              );
             }
-            else {
-                let id = judge[0].judgeID
-                database.query(`select fieldNumber from field where '${data.sportField}' = fieldName`,(err,fieldNum)=>{
-                    let field
-                    if(fieldNum.length < 0) field = null
-                    else field = parseInt(fieldNum[0].fieldNumber) 
-                    database.query('INSERT INTO `match`(`judgeID`, `nameTeam1`, `nameTeam2`, `nameTeam3`, `scoreTeam1`, `scoreTeam2`, `scoreTeam3`, `team1Player`, `team2Player`, `team3Player`, `sportType`, `sportCategory`, `fieldNumber`, `matchStartTime`, `matchEndTime`) VALUES ('+id+',"'+data.nameTeam1+'","'+data.nameTeam2+'","'+data.nameTeam3+'","'+data.scoreTeam1+'" ,"'+data.scoreTeam2+'","'+data.scoreTeam3+'","'+data.player1+'","'+data.player2+'","'+data.player3+'","'+data.sportType+'","'+data.sportCategory+'",'+field+',"'+data.startTime+'" ,"'+data.endTime+'")'
-                    )
-                })
-            }
-                       
-        })
-    },
-    getScorebyFilter(req,res,next){
-        console.log(req.params);
-        const Sport = req.params.sport;
-        const Day = req.params.Day;
-        var returnArr = []
-        database.query('select * from `match`',(err,data)=>{
-            if(data.length > 0){
-                data.forEach(each=>{
-                    const sportQuery = each.sportType.toUpperCase() , dayQuery = each.matchStartTime.toString().substr(4, 6)
-                    if(sportQuery == Sport && dayQuery == Day) returnArr.push(each)
-                })
-            }
-            res.send(returnArr)
-        })
-        
-    }
-}
+          );
+        }
+      }
+    );
+  },
+  getScorebyFilter(req, res, next) {
+    console.log(req.params);
+    const Sport = req.params.sport;
+    const Day = req.params.Day;
+    var returnArr = [];
+    database.query("select * from `match`", (err, data) => {
+      if (data.length > 0) {
+        data.forEach(each => {
+          const sportQuery = each.sportType.toUpperCase(),
+            dayQuery = each.matchStartTime.toString().substr(4, 6);
+          if (sportQuery == Sport && dayQuery == Day) returnArr.push(each);
+        });
+      }
+      res.send(returnArr);
+    });
+  },
+  getmedal(req, res, next) {
+    const data = req.body.data;
+    database.query("select * from medal", (err, data) => {
+      res.json(data);
+    });
+  }
+};
